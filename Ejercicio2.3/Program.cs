24be4df3ddaf59a33504cb85fd14daed67de53c7
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 15:36:11 -0400

    Ejercicios

---
+++ Ejercicio2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada = "";
+            double Grados, resultado=0;
+            double P = 3.1416;
+
+            Console.Write("Ingrese los grados a convertir: ");
+            entrada = Console.ReadLine();
+            Grados = Convert.ToDouble(entrada);
+
+            resultado = Grados * P / 180;
+
+            Console.WriteLine("\nEl resultado es: {0} radianes", resultado);
+        }
+    }
+}
