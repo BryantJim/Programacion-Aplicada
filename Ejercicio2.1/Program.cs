af8e5a3400942f376129d4a8c55e064a570b4efd
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 15:20:16 -0400

    Ejercicios

---
+++ Ejercicio2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada = "";
+            int NumeroLados,Longitud,resultado=0;
+
+            Console.Write("Ingrese el numero de lados del poligono: ");
+            entrada = Console.ReadLine();
+            NumeroLados = Convert.ToInt32(entrada);
+
+            Console.Write("Ingrese la longitud de un lado del poligono: ");
+            entrada = Console.ReadLine();
+            Longitud = Convert.ToInt32(entrada);
+
+            resultado = NumeroLados * Longitud;
+
+            Console.WriteLine("\nEl resultado es: {0}",resultado);
+        }
+    }
+}
