119a1bd0a83f26d053e49d57550b30171eb58fe1
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 16:31:27 -0400

    Ejercicios

---
+++ Ejercicio2.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Entrada = "";
+            double Dolar = 0, Euro = 0, TasaDolar = 0, TasaEuro = 0, Dolares = 0, Euros = 0;
+
+            Console.Write("Cantidad de dolares a convertir a Euro: ");
+            Entrada = Console.ReadLine();
+            Dolar = Convert.ToDouble(Entrada);
+
+            Console.Write("Cantidad de Euro a convertir a dolares: ");
+            Entrada = Console.ReadLine();
+            Euro = Convert.ToDouble(Entrada);
+
+            Console.Write("Tasa del dolar a euro: ");
+            Entrada = Console.ReadLine();
+            TasaDolar = Convert.ToDouble(Entrada);
+
+            Console.Write("Tasa del euro a dolar: ");
+            Entrada = Console.ReadLine();
+            TasaEuro = Convert.ToDouble(Entrada);
+
+            Dolares = Dolar * TasaEuro;
+            Euros = Euro * TasaDolar;
+
+            Console.WriteLine("De dolar a Euro: {0}",Euros);
+            Console.WriteLine("De Euro a Dolar: {0}",Dolares);
+        }
+    }
+}
