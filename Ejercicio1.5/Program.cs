80a97937576da3a3e2db62a392fb8bbb14ab2be7
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 14:47:53 -0400

    Ejercicios Cap 1

---
+++ Ejercicio1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nombre es: Anthony Brian Jimenez Lopez");
+            Console.WriteLine("Soy estudiante de Ing.en Sistema en la UCNE");
+            Console.WriteLine("Gracias por visitar mi perfil");
+        }
+    }
+}
