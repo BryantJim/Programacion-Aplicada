b96e134c197699b2c029618499d6a9a7eb82c0ad
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 14:31:11 -0400

    Ejercicios

---
+++ Ejercicio1.1/Ejercicio1.1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nombre es: Anthony Brian Jimenez Lopez");
+        }
+    }
+}
