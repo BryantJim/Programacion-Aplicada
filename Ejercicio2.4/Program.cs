7636db004e620babb5e87bd62438b1d4cc0d7973
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-18 15:48:33 -0400

    Ejercicio

---
+++ Ejercicio2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String entrada = "";
+            double GradosCelsius, GradosFahrenheit=0;
+
+            Console.Write("Ingrese la cantidad de grado celsius: ");
+            entrada = Console.ReadLine();
+            GradosCelsius = Convert.ToDouble(entrada);
+
+            GradosFahrenheit = GradosCelsius * 9 / 5 + 32;
+
+            Console.WriteLine("\nEl resultado en Grados Fahrenheit: {0}", GradosFahrenheit);
+        }
+    }
+}
